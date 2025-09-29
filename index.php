a76f43f8a1eb4bdab6ddf0f5fde4cca966329ffa
Marjan ghazi <syedmarjanghazi@gmail.com>
2025-09-29 19:56:54 +0500

    updated

--- index.php
+++ index.php
@@ -654,6 +654,10 @@
         <li class="nav-item">
           <a class="nav-link" href="#contact">Contact</a>
         </li>
+          <li class="nav-item">
+          <a class="nav-link" href="./wordpress/">Blogs</a>
+        </li>
+
       </ul>
     </div>
   </div>
